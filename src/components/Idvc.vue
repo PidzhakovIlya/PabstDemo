a59c634f5345683d05e6723af4b8ed48b7594985
PidzhakovIlya <pidzhakovilja@gmail.com>
2024-11-29 09:36:34 +0300

    delete license key

--- src/components/Idvc.vue
+++ src/components/Idvc.vue
@@ -24,33 +24,7 @@ export default {
     let idvc = new IDVC({
       el: "videoCapturingEl",
       captureTimeout: 100,
-      // licenseKey: "LICENSE-KEY",
-      licenseKey:
-        "eyJwZGY0MTdrZXkiOiJVNjZyRmJldkRmXHUwMDJCdXJCL3g0TUdBTHJXZ0" +
-        "JlUVZ4NUZ1R1FpS3pGMlJIM0RweXF0MmtHN2Z0RXl6TlpkZEFUOUpMbmVBW" +
-        "jlTS25KOGhKUFFLdnNCMWlXZ2c0VkphMUg3aGozak9xRnBDVFBjcU0vMzV4S" +
-        "k4yNW1vMjJiY1x1MDAyQlExSU5cdTAwMkJYNjc1MG5oajZTTWdselBEL0tBN" +
-        "E9rejZRandmZXJsWlZKeHFuYWtqY0U9IiwiY29tbW9uTGljZW5zZUtleSI6I" +
-        "kNGOGlVN0Zpb2pPZndrbnZBZDhQamFoREdacXIxVXB5VlJDUUhxSHRGcUQwW" +
-        "C9RRGEzc0xPOFliSFZ4TVx1MDAyQlNsOUhzT3dvYzFzRlJnRWdWMUZCdXY5b" +
-        "3hJSlRvSC84SnNzTXMzQWhIUnRjM2RkSFRsNzhwYkl2VTZ1bW84V2xXdEVsW" +
-        "llJMTdJZHduSFx1MDAyQlZ2YjU3NXV5SVB1NXQvM0VNQ05Id1ZuN1Z0dzJBV1" +
-        "E9IiwiaW1hZ2VQcm9jZXNzaW5nS2V5Ijoibmt2d0ZDbG9TMnJvMkhEb1F3OXN" +
-        "2V0lrdnh4dm1ZQkZIOGtURHdTTzhBcUVwWXZDYjZcdTAwMkJOMmhnSWZoV05P" +
-        "QlVYNE5VRTJCSGpLV3ovc3dsYkZKV0FqZVF4Y3R5bTFTRGNMTHhVMllsOExxQ" +
-        "kJTVWZla1J1Vm45NkQvOHRwSFkzVFZ2VzVRSFpBYUJTc3lTRURJZUd0UFZKaX" +
-        "VGaWJJajVoaEJoVm9WNXg0cTg9IiwidHJhY2tTdHJpbmdQYXJzZXJLZXkiOiJ" +
-        "uRUhYVE1NMUVYXHUwMDJCRlp0N2thSHFkOUFLRGlBSjlWdXVzcngvRk9YUVx1" +
-        "MDAyQjlBVEl0WFJ2bUxzY3pUMkFjQzdEdkpoa29CNldzMG5NN1Uzd0cvbnhJM" +
-        "2Z5aVFxMmVXUTlYZXRDTi9ESHpCbTdmZmNoUG84QlFlXHUwMDJCRFBLWmdTL3" +
-        "ZkSUc1cDVudnZzTFZNNDZoeDdURGx3Z2g0cm9yMlZoeFF3R0VWamNOVzRZb28" +
-        "3TFU9IiwiaU9TTVJaRGV0ZWN0b3JLZXkiOiIwbDVSejNwbGlRN253Q2dcdTAw" +
-        "MkJSMW12ck9VVVx1MDAyQmhhNFhMNnN6S3FpL05uT3dYcEQ1V3R0cjVGOVlvN" +
-        "nRlWFdlaHdJVkdLTzN3VG5rYWpNWThcdTAwMkJSaXdGTmFoV1dtTkxcdTAwMk" +
-        "JVMlliWVAvNWtWUm5Qc0lSbVx1MDAyQklPOUZlN1FoUTFuMGxwbjRnNlUvYlx" +
-        "1MDAyQndRV1R5TEliXHUwMDJCbEs4UVRsZVNaTmhtT1BGcUh4MXFmWlZFR052" +
-        "ZS9zST0iLCJpT1NQREZEZXRlY3RvcktleSI6ImxNb1BjT2FMbFBoS29Kc050Q" +
-        "nFIdEF3Q2NJcktyRGdKIn0=",
+      licenseKey: "LICENSE-KEY",
       networkUrl: "networks",
       resizeUploadedImage: 1600,
       fixFrontOrientAfterUpload: true,
